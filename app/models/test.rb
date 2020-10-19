ffb846c5457cca9dd860c6bc0790f45162b460e5
Andrew <Andreymg184@gmail.com>
2020-10-19 11:43:20 +0600

    add validations, scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,12 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passages
   belongs_to :author, class_name: 'User'
 
-  def self.by_category(title)
-    id = Category.find_by(title: title).id
-    Test.where(category_id: id).order('id DESC')
-  end
+  validates :title, uniqueness: { scope: :level }
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true
+
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, -> (title) { where(category: Category.find_by(title: title)) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id'
 
+  validates :fname, :lname, :email,  presence: true
+
   def tests_by_level(level)
     tests.where(level: level).order('id DESC')
   end
